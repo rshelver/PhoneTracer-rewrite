a60ace92e115ff197f1324fe3a5d651004faf1a8
rshelver <rshelver@outlook.com>
2020-09-23 13:19:36 -0700

    Updated to 1.0.1
    updated all selectors and inputs

--- PhoneTracer.js
+++ PhoneTracer.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer')
 const readline = require('readline')
 
+
 var rl = readline.createInterface({
 input: process.stdin,
 output: process.stdout
@@ -9,11 +10,11 @@ output: process.stdout
 
 phone_number = 0;
 
-console.log("PhoneTracer-Rewrite 1.0");
+console.log("PhoneTracer-Rewrite 1.0.1");
 console.log("Developed by Mutiny27");
 
 rl.question("Please Enter the number you wish to trace: ", function(answer) {
-  phone_number = answer
+  phone_number = answer;
   console.log("Please wait...");
   rl.close();
 });
@@ -24,12 +25,13 @@ const main = async () => {
   await page.setViewport({ width: 1366, height: 768});
   await page.goto('https://www.spydialer.com/', {waitUntil: 'networkidle2'})
 
-  await page.type('#ctl00_ContentPlaceHolder1_SearchInputTextBox', phone_number)
+  await page.type('#SearchInputTextBox', phone_number)
+
 
   await page.keyboard.press('Enter');
 
 
-  await page.waitForSelector('#ctl00_ContentPlaceHolder1_SearchCellImageButton')
+  await page.waitForSelector('[name="ctl00$ContentPlaceHolder1$SearchCellImageButton"]')
 
   setTimeout(() => {page.click('[name="ctl00$ContentPlaceHolder1$SearchCellImageButton"]'); }, 7000);
 
@@ -39,6 +41,7 @@ const main = async () => {
   const elementId = 'ctl00_ContentPlaceHolder1_NameLinkButton';
 
   const text = await page.evaluate(elementId => document.getElementById(elementId).textContent, elementId);
+
 
   console.log("name:", text);
 
